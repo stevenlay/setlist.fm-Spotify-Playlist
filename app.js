6c82628584d390a599513db15287f59f167abf44
Steven Lay <stlay@ucsd.edu>
2017-10-16 16:56:10 +0000

    bodyparser added

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ var express = require('express'),
     request = require('request'),
     querystring = require('querystring'),
     cookieParser = require('cookie-parser'),
-    client = require('./lib/client.js');
+    client = require('./lib/client.js'),
+    bodyParser = require('body-parser');
 
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended:true}));
 var client_id = client.client_id;
 var client_secret = client.client_secret;
 
 app.get("/", function(req, res) {
-    res.render("search.ejs");
+    res.render("search");
 });
 
 app.post("/", function(req, res) {
-    res.send("POST ROUTE");
+    res.send(req.body.artist + " " + req.body.tour);
 });
 
 
