5a7fb26952b9ec3971686ee20164c5bc9e9e2efe
Steven Lay <stlay@ucsd.edu>
2017-10-16 20:48:29 +0000

    added post route

--- app.js
+++ app.js
@@ -8,14 +8,36 @@ var express = require('express'),
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
-var client_id = client.client_id;
-var client_secret = client.client_secret;
+var client_id = client.client_id
+    client_secret = client.client_secret,
+    api_key = client.api_key;
+
 
 app.get("/", function(req, res) {
     res.render("search");
 });
 
 app.post("/", function(req, res) {
+    var headers = {
+        'Accept': 'application/json',
+        'x-api-key': api_key
+    };
+    
+    var options = {
+        url: 'https://api.setlist.fm/rest/1.0/search/setlists?artistName=' + req.body.artist + '&p=1&tourName=' + req.body.tour,
+        headers: headers
+    };
+    
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            (data['setlist'].forEach(function(set) {
+                console.log(set.artist.name);    
+            }));
+        }
+    }
+    
+    request(options, callback); 
     res.send(req.body.artist + " " + req.body.tour);
 });
 
