38b83a22caca86cfbba6e102da0f124de38dcc1a
Steven Lay <stlay@ucsd.edu>
2017-10-16 16:44:07 +0000

    form

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-var express = require('express');
-var request = require('request');
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
+var express = require('express'),
+    app = express(),
+    request = require('request'),
+    querystring = require('querystring'),
+    cookieParser = require('cookie-parser'),
+    client = require('./lib/client.js');
 
-var client = require('./lib/client.js');
+app.set("view engine", "ejs");
+
 var client_id = client.client_id;
 var client_secret = client.client_secret;
 
-console.log(client_id);
+app.get("/", function(req, res) {
+    res.render("search.ejs");
+});
+
+app.post("/", function(req, res) {
+    res.send("POST ROUTE");
+});
+
+
+app.listen(8080, function() {
+    console.log("listening on 8080");
+});
+
+
